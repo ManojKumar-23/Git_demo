f964e749a7b7d0316c2667de33832e97324620d8
ManojKumar-23 <60685220+ManojKumar-23@users.noreply.github.com>
2020-07-28 15:27:39 +0530

    Add files via upload

---
+++ Multi.java
@@ -0,0 +1,15 @@
+public class Multi {
+
+	public static void main(String[] args) {
+		int first = 10;
+       		int second = 20;
+
+      		System.out.println("Enter two numbers: " + first + " " + second);
+        	int mul = first * second;
+
+	        System.out.println("The multiplication is: " + mul);
+
+		//System.out.println("Hello World!!!");
+
+	}
+}
